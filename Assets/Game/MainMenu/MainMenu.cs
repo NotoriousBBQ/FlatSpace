cf190ee60e4b4d3502abf416a9984489fb6a3aab
NotoriousBBQ <chesler_lex@yahoo.com>
2025-09-14 18:23:17 -0700

    Save and options button are non-interactable upon start

--- Assets/Game/MainMenu/MainMenu.cs
+++ Assets/Game/MainMenu/MainMenu.cs
@@ -1,6 +1,14 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Button _startButton;
+    [SerializeField] private Button _saveButton;
+    [SerializeField] private Button _loadButton;
+    [SerializeField] private Button _optionsButton;
 
     public void StartGame()
     {
@@ -15,5 +23,13 @@ public class MainMenu : MonoBehaviour
     public void LoadGame()
     {
         SaveLoadSystem.LoadGame();
+    }
+
+    private void Awake()
+    {
+        if(_saveButton != null)
+            _saveButton.interactable = false;
+        if (_optionsButton != null)
+            _optionsButton.interactable = false;
     }
 }
